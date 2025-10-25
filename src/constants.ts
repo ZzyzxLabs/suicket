508531e6dfbba57934cdb3ac4df1213ad64c191b
Thomas Lee <thomas930131@gmail.com>
2025-10-25 15:31:22 -0700

    update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,10 @@
 // CHANGED: Updated to support ticket NFT system
 // These will be populated after deploying the Move contract
 export const DEVNET_COUNTER_PACKAGE_ID = import.meta.env.VITE_PACKAGE_ID || "0xTODO";
-export const TESTNET_SUICKET_PACKAGE_ID = "0x349027fd90cee4de6148900d6ad649e6813b9d13eada6405c9a6d72f8b12a4cd";
+export const DEVNET_SUICKET_PACKAGE_ID = import.meta.env.VITE_PACKAGE_ID || "0xTODO";
+export const TESTNET_SUICKET_PACKAGE_ID = "0x832f70062c46e139d2191d1901eae9b51744c5a84e774f140bff00139669f787";
 export const MAINNET_COUNTER_PACKAGE_ID = "0xTODO";
+export const MAINNET_SUICKET_PACKAGE_ID = "0xTODO";
 
 // Shared EventCounter object ID (set after deployment)
 export const DEVNET_COUNTER_ID = import.meta.env.VITE_COUNTER_ID || "";
